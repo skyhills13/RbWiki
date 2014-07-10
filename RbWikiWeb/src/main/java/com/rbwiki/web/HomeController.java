85909ada8bc7063f2df4c8542ff51f53eec687d9
skyhills13 <skyhills13@gmail.com>
2014-07-11 02:25:31 +0900

    add logging framework slf4j logging framework 적용

--- RbWikiWeb/src/main/java/com/rbwiki/web/HomeController.java
+++ RbWikiWeb/src/main/java/com/rbwiki/web/HomeController.java
@@ -1,12 +1,19 @@
 package com.rbwiki.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping("/")
 	public String home(){
+		//logger.debug("debug level, into home"); //이것은 debug level이라 콘솔에 보이지 않음. 어디서 볼수있게? 
+		logger.info("into home");
+		
 		return "home";
 	}
 }
