f7aae6ccf42841b8b01c65b7c319ac10400008e4
skyhills13 <skyhills13@gmail.com>
2014-07-20 21:34:11 +0900

    UserController와 UserDao 연

--- RbWikiWeb/src/main/java/com/rbwiki/web/users/UserController.java
+++ RbWikiWeb/src/main/java/com/rbwiki/web/users/UserController.java
@@ -2,23 +2,31 @@ package com.rbwiki.web.users;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.rbwiki.dao.users.UserDao;
 import com.rbwiki.domain.users.User;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
-	@RequestMapping("/users/form")
+	@Autowired
+	private UserDao userDao; 
+	
+	@RequestMapping("/form")
 	public String form(){
 		return "users/form";
 	}
-	@RequestMapping(value="/users", method=RequestMethod.POST)
+	@RequestMapping(value="", method=RequestMethod.POST)
 	public String create(User user){
 		logger.debug("User : {}", user);
+		userDao.create(user);
+		logger.debug("Database: {}", userDao.findById(user.getUserId()));
 		return "users/form";
 	}
 }
